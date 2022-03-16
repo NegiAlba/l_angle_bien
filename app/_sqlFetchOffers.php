f496157d5f3804ef1380fd65c12c1c27dd7dd3cb
Axel Gromat <axel.gromat@gmail.com>
2022-03-16 10:05:05 +0100

    Changing pattern to functional pattern with a router

--- app/_sqlFetchOffers.php
+++ app/_sqlFetchOffers.php
@@ -1,13 +1,20 @@
 <?php
 
-// ? Requête SQL qui permet de récupérer les offres (sauf celles du user connecté)
-try {
-    $sqlOffers = 'SELECT * FROM offers AS off INNER JOIN categories AS cat ON cat.categories_id = off.category_id WHERE off.author_id <> :user';
-    $reqOffers = $db->prepare($sqlOffers);
-    $reqOffers->bindValue(':user', $user);
-    $reqOffers->execute();
+function fetchOffers(int $onlineUser): array
+{
+    global $db;
 
-    $offers = $reqOffers->fetchAll();
-} catch (PDOException $e) {
-    echo 'Erreur :'.$e->getMessage();
+    // ? Requête SQL qui permet de récupérer les offres (sauf celles du user connecté)
+    try {
+        $sqlOffers = 'SELECT * FROM offers AS off INNER JOIN categories AS cat ON cat.categories_id = off.category_id WHERE off.author_id <> :user';
+        $reqOffers = $db->prepare($sqlOffers);
+        $reqOffers->bindValue(':user', $onlineUser);
+        $reqOffers->execute();
+
+        return $reqOffers->fetchAll();
+    } catch (PDOException $e) {
+        echo 'Erreur :'.$e->getMessage();
+    }
 }
+
+$offers = fetchOffers($user);
--- app/includes/db.php
+++ app/includes/db.php
@@ -2,16 +2,25 @@
 
     require 'local.env.php';
 
-    $db_string = 'mysql:dbname='.DBNAME.';host='.HOST;
+    function db(string $dbname, string $host, string $user, string $password): PDO
+    {
+        $db_string = "mysql:dbname=$dbname;host=$host";
 
-    //? Un bloc try-catch est une instruction de code qui permet de gérer explicitement les erreurs qui pourraient advenir.
-    try {
-        //? PDO = PHP Data Object , c'est un objet qui a vocation a représenter une connexion entre une application PHP et un système de base de données. PDO support MySQL, PostGres.
-        $db = new PDO($db_string, USER, PASSWORD);
-        //? setAttribute permet de changer les paramètres par défaut de PDO. Il nous permet de définir des comportements qui doivent s'appliquer en tout temps. On modifie les comportements de renvoi d'erreur (on souhaite toujours des exceptions PDO) et celui de fetching (la récupération depuis la BDD) avec des tableaux associatifs (plutôt que associatif + indexé).
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        echo 'Erreur : '.$e->getMessage();
-        // throw new MyPDODbException($e);
+        // ? Un bloc try-catch est une instruction de code qui permet de gérer explicitement les erreurs qui pourraient advenir.
+        try {
+            // ? PDO = PHP Data Object , c'est un objet qui a vocation a représenter une connexion entre une application PHP et un système de base de données. PDO support MySQL, PostGres.
+            $db = new PDO($db_string, $user, $password);
+            // ? setAttribute permet de changer les paramètres par défaut de PDO. Il nous permet de définir des comportements qui doivent s'appliquer en tout temps. On modifie les comportements de renvoi d'erreur (on souhaite toujours des exceptions PDO) et celui de fetching (la récupération depuis la BDD) avec des tableaux associatifs (plutôt que associatif + indexé).
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            // Vu que l'appel à la BDD est dans une fonction, je dois la retourner
+            return $db;
+        } catch (PDOException $e) {
+            echo 'Erreur : '.$e->getMessage();
+            // throw new MyPDODbException($e);
+        }
     }
+
+    // ? Notre variable $db correspond à l'appel à la BDD, il sera disponible comme avant dans la vrible $db
+    $db = db(DBNAME, HOST, USER, PASSWORD);
